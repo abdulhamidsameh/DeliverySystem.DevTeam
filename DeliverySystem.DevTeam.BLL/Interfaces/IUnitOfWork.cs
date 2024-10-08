344d2fcf755739cee547e04a61ec1726b13c0543
Im-kamal <ka856248@gmail.com>
2024-10-08 16:53:04 +0300

    Add GenericRepository and UnitOfWork

---
+++ DeliverySystem.DevTeam.BLL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using DeliverySystem.DevTeam.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliverySystem.DevTeam.BLL.Interfaces
+{
+	public interface IUnitOfWork : IDisposable
+	{
+
+		IGenericRepository<T> Repository<T>() where T : BaseEntity;
+		int Complete();
+    }
+}
