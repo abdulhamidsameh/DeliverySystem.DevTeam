20b4a495b6831e47b1ee730fdc6b7bc80dabdec1
ahmedfarouk22277@gmail.com <ahmedfarouk22277@gmail.com>
2024-10-10 17:56:21 +0300

    u

---
+++ DeliverySystem.DevTeam.BLL/Specifications/BaseSpacefications.cs
@@ -0,0 +1,20 @@
+﻿using DeliverySystem.DevTeam.DAL.Models;
+using System.Linq.Expressions;
+namespace DeliverySystem.DevTeam.BLL.Specifications
+{
+	public class BaseSpacefications<T> : ISpecification<T> where T : BaseEntity
+	{
+		public BaseSpacefications(Expression<Func<T, bool>> criteriaExpression)
+		{
+			Criteria = criteriaExpression;
+		}
+		public BaseSpacefications()
+		{
+
+		}
+
+
+		public Expression<Func<T, bool>> Criteria { get; set; } = null!;
+		public List<Expression<Func<T, object>>> Includes { get; set; } = new List<Expression<Func<T, object>>>();
+	}
+}
