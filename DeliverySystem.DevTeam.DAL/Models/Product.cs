9fd2c2747b1163faaa6858b6782a3002b39cd481
Im-kamal <ka856248@gmail.com>
2024-10-10 17:03:59 +0300

    up

--- DeliverySystem.DevTeam.DAL/Models/Product.cs
+++ DeliverySystem.DevTeam.DAL/Models/Product.cs
@@ -4,7 +4,7 @@ namespace DeliverySystem.DevTeam.DAL.Models
 	public class Product
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
+		public string Name { get; set; }  
 		public string Description { get; set; }
 		public bool IsDeleted { get; set; }
 		public decimal Price { get; set; }  
