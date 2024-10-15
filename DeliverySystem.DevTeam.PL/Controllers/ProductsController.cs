f55360a44faa76c89bb51d679b55dfddfe5df52a
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-10-15 16:49:35 +0300

    Use Spaceficaation

--- DeliverySystem.DevTeam.PL/Controllers/ProductsController.cs
+++ DeliverySystem.DevTeam.PL/Controllers/ProductsController.cs
@@ -2,169 +2,167 @@
 
 
 
+using DeliverySystem.DevTeam.BLL.Specifications;
 using DeliverySystem.DevTeam.DAL.Models;
 
 namespace DeliverySystem.DevTeam.PL.Controllers
 {
-    public class ProductsController : Controller
-    {
+	public class ProductsController : Controller
+	{
 
-        public ProductsController( IMapper mapper ,IUnitOfWork unitOf,ApplicationDbContext context)
-        {
-          
-            _Mapper = mapper;
+		public ProductsController(IMapper mapper, IUnitOfWork unitOf, ApplicationDbContext context)
+		{
+
+			_Mapper = mapper;
 			_UnitOf = unitOf;
 			_Context = context;
 		}
 
-      
-        public IMapper _Mapper { get; }
+
+		public IMapper _Mapper { get; }
 		public IUnitOfWork _UnitOf { get; }
 		public ApplicationDbContext _Context { get; }
 
 		public IActionResult Index()
-        {
-            var Products = _UnitOf.Repository<Product>().GetAll();
+		{
+			var Products = _UnitOf.Repository<Product>().GetAll();
 
-            return View(Products);
-        }
+			return View(Products);
+		}
 
 
-        #region Add Product
-        [HttpGet]
-        [AjaxOnly]
-        public IActionResult Create()
-        {
-            var Warhouse = _UnitOf.Repository<Warhouse>().GetAll();
-            var Result = new CreateOrUodateProductViewModel()
-            {
+		#region Add Product
+		[HttpGet]
+		[AjaxOnly]
+		public IActionResult Create()
+		{
+			var Warhouse = _UnitOf.Repository<Warhouse>().GetAll();
+			var Result = new CreateOrUodateProductViewModel()
+			{
 
-                Warhouses = Warhouse.ToList(),
-            };
+				Warhouses = Warhouse.ToList(),
+			};
 			return PartialView("_Form", Result);
-        }
-        [HttpPost]
-        public IActionResult Create(CreateOrUodateProductViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-            
-                var product = _Mapper.Map<Product>(model);
+		}
+		[HttpPost]
+		public IActionResult Create(CreateOrUodateProductViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+
+				var product = _Mapper.Map<Product>(model);
 
 				_UnitOf.Repository<Product>().Add(product);
 				//TempData["message"] = "Saved SuccessFully";
 
 				_UnitOf.Complete();
-                return PartialView("_ProductRow", product);
+				return PartialView("_ProductRow", product);
 
-            }
-            else
-            {
-                return View("_Form", model);
-            }
-
-
-
-        }
-        #endregion
+			}
+			else
+			{
+				return View("_Form", model);
+			}
 
 
-        #region Edit
 
-        [HttpGet]
-        [AjaxOnly]
-
-        public IActionResult Edit(int id)
-        {
-            var Product = _UnitOf.Repository<Product>().GetById(id);
-            var Warhouse = _UnitOf.Repository<Warhouse>().GetAll();
+		}
+		#endregion
 
 
-            if (Product != null)
-            {
-                var Result = new CreateOrUodateProductViewModel()
-                {
-                    Description = Product.Description,
-                    Id = Product.Id,
-                    Name = Product.Name,
-                    Price = Product.Price,
-                    QuantityAvailable = Product.QuantityAvailable,
-                    Warhouses = Warhouse.ToList(),
-                    WarhouseId = Product.WarhouseId
+		#region Edit
+
+		[HttpGet]
+		[AjaxOnly]
+
+		public IActionResult Edit(int id)
+		{
+			var Product = _UnitOf.Repository<Product>().GetById(id);
+			var Warhouse = _UnitOf.Repository<Warhouse>().GetAll();
 
 
-                };
+			if (Product != null)
+			{
+				var Result = new CreateOrUodateProductViewModel()
+				{
+					Description = Product.Description,
+					Id = Product.Id,
+					Name = Product.Name,
+					Price = Product.Price,
+					QuantityAvailable = Product.QuantityAvailable,
+					Warhouses = Warhouse.ToList(),
+					WarhouseId = Product.WarhouseId
 
-                return PartialView("_Form", Result);
-            }
+
+				};
+
+				return PartialView("_Form", Result);
+			}
 
 
-            return NotFound(); 
+			return NotFound();
 
-        }
+		}
 
-        [HttpPost]
-        public IActionResult Edit(CreateOrUodateProductViewModel model)
-        {
+		[HttpPost]
+		public IActionResult Edit(CreateOrUodateProductViewModel model)
+		{
 			var Product = _UnitOf.Repository<Product>().GetById(model.Id);
 
 			if (Product != null)
-            {
-                Product.Price = model.Price;
-                Product.Name = model.Name;
-                Product.QuantityAvailable = model.QuantityAvailable;
-                Product.Description = model.Description;
-                Product.LastUpdatedOn = DateTime.Now;
-                Product.WarhouseId = model.WarhouseId;
+			{
+				Product.Price = model.Price;
+				Product.Name = model.Name;
+				Product.QuantityAvailable = model.QuantityAvailable;
+				Product.Description = model.Description;
+				Product.LastUpdatedOn = DateTime.Now;
+				Product.WarhouseId = model.WarhouseId;
 				_UnitOf.Complete();
 
 				//TempData["message"] = "Saved SuccessFully";
 
 				return PartialView("_ProductRow", Product);
 
-            }
+			}
 
 
-            return NotFound();
+			return NotFound();
 
-        }
-        #endregion
-
-
-        #region Toggle Status 
+		}
+		#endregion
 
 
-        public IActionResult ToggleStatus(int id)
-        {
+		#region Toggle Status 
+
+
+		public IActionResult ToggleStatus(int id)
+		{
 
 
 			var product = _UnitOf.Repository<Product>().GetById(id);
 
 			if (product == null) { return NotFound(); };
 
-            product.IsDeleted = !product.IsDeleted;
-            product.LastUpdatedOn = DateTime.Now;
+			product.IsDeleted = !product.IsDeleted;
+			product.LastUpdatedOn = DateTime.Now;
 			_UnitOf.Complete();
 
 
 			return Ok(product.LastUpdatedOn.ToString());
-        }
+		}
 
 
-        #endregion
+		#endregion
 
 
 
-        public IActionResult Productdetails(int id)
-        {
-            //var Warhouse = _UnitOf.Repository<Warhouse>().GetById(id);
-            var Warhouse = _Context.Warehouses.Where(c => c.Id == id).Include(x => x.Products).ToList();
+		public IActionResult Productdetails(int id)
+		{
+			var spec = new BaseSpacefications<Warhouse>(W => W.Id == id);
+			spec.Includes.Add(W => W.Products);
+			var Warhouse = _UnitOf.Repository<Warhouse>().GetAllWithSpec(spec).ToList();
 			return View(Warhouse);
-        }
-
-
-
-
+		}
 	}
 
 }
