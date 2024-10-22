f8dddbbc2e233388ce9e3377a6cf3b8e2916cd3d
Im-kamal <ka856248@gmail.com>
2024-10-23 02:44:50 +0300

    Add WarehouseStaff Controller

---
+++ DeliverySystem.DevTeam.PL/Controllers/WarehouseStaffController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliverySystem.DevTeam.PL.Controllers
+{
+    public class WarehouseStaffController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public WarehouseStaffController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var orders = _unitOfWork.Repository<Order>().GetAll();
+            return View(orders);
+        }
+
+        public IActionResult GetOrderDetails(int id)
+        {
+            var order = _unitOfWork.Repository<Order>().GetById(id);
+            if(order is null) 
+                return NotFound();
+            return View(order);
+        }
+        [HttpPost]
+        public IActionResult ConfirmOrder(int id)
+        {
+            var order = _unitOfWork.Repository<Order>().GetById(id);
+            if (order is null)
+                return NotFound();
+            order.OrderStatus = OrderStatus.Processing;
+            _unitOfWork.Repository<Order>().Update(order);
+            _unitOfWork.Complete();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
